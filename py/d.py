afae5b73c3e8dd9fbd656845c487193be973b006
chaodeng <645111208@qq.com>
2018-07-13 10:49:35 +0800

    add py

---
+++ py/d.py
@@ -0,0 +1,41 @@
+# print("hello world!")
+# print("100+200=", 100 + 200)
+
+# 输入
+# # name = input("plase input your name:")
+# # print("hello ,", name, "!")
+# print(["可变字符串", "01", "02"])
+
+# 条件
+# age = 17
+# if age >= 18:
+#   print("adult")
+# elif age >= 6:
+#   print("teenager")
+# else:
+#   print("kid")
+
+# 输入字符串转码成int()
+# year = int(input())
+# if (year % 400 == 0) or (year % 100 != 0 and year % 4 == 0):
+#   print("闰年")
+# else:
+#   print("平年")
+
+# 测试是否肥胖
+# print("BMI检测开始:")
+# sg = int(input('请输入身高(Cm)'))
+# sg = sg / 100
+# tz = int(input('请输入体重(Kg)'))
+# BMI = tz / (sg * sg)
+# print('您的BMI指数为:', '%.1f' % BMI)
+# if BMI < 18.5:
+#   print('过轻')
+# elif BMI >= 18.5 and BMI <= 25:
+#   print('正常')
+# elif BMI > 25 and BMI <= 28:
+#   print('过重')
+# elif BMI > 28 and BMI <= 32:
+#   print('肥胖')
+# elif BMI > 32:
+#   print('严重肥胖')
