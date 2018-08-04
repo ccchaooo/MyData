9e2969c89f9db894686d4288914597f5c955edbd
chaodeng <645111208@qq.com>
2018-08-04 11:34:43 +0800

    提交

--- EDU/src/js/post/postdemo.js
+++ EDU/src/js/post/postdemo.js
@@ -1,25 +1,28 @@
+$.post("../xmbj/queryDWXX.do", {
+    "jsonStr": jsonInfo
+  },
+  function(resp) {
+    if (resp.flag * 1 == 0) {
+      layer.alert(
+        "保存成功！", {
+          icon: 1
+        },
+        function() {
+          if (test) {
+            location.href = "/cdjwztyypt/xmbj/xmbj_bdxx.jsp?xmid=" + xmid + "&bdid=" + bdid;
+          } else {
+            location.href = "/cdjwztyypt/xmbj/xmbj_bdxx_list.jsp?xmid=" + __xmid;
+          }
+        }
+      );
 
-			        $.post("../xmbj/saveBdxx.do",
-               {"jsonStr":jsonInfo},
-               function(resp){
-                  if(resp.flag*1 == 0){
-						layer.alert(
-									"保存成功！",
-									{icon:1},
-									function(){
-												if(test){
-												location.href="/cdjwztyypt/xmbj/xmbj_bdxx.jsp?xmid="+xmid+"&bdid="+bdid;
-												}else{
-												location.href="/cdjwztyypt/xmbj/xmbj_bdxx_list.jsp?xmid="+__xmid;
-												}
-											  }
-									);
-						
-				 }else{	
-						layer.alert(resp.msg,{icon:7});
-						}
-						scope.$apply(function(){
-						    scope.if_next=false;				
-						});
-               },
-            "json");
+    } else {
+      layer.alert(resp.msg, {
+        icon: 7
+      });
+    }
+    scope.$apply(function() {
+      scope.if_next = false;
+    });
+  },
+  "json");
--- EDU/src/jsp模板/ajax.jsp
+++
@@ -1,5 +0,0 @@
-<select select2-cdmcs="initInfo.N_STJG" class="form-control" name="N_STJG" ng-model="stxxlrInfo.N_STJG"
-        data-ajax--url="<%=basePath%>xmbj/querySTJG.do" data-ajax--cache="true"
-        data-ajax--method="post" data-ajax--delay="500"
-        ng-required="true" data-placeholder="输入单位名称关键字搜索">
-</select>
