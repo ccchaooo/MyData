2a1ced32c88579ce71e395d21e7c535f3e17a5f7
chaodeng <645111208@qq.com>
2018-09-03 10:14:31 +0800

    "ss"

---
+++ EDU/src/js/grid/gird link.js
@@ -0,0 +1,7 @@
+                        "  DECODE(JL.C_ZT,\n" +
+                        "        '0',\n" +
+                        "        '修改^javascript:f_op(row-'||rownum||',true)^_self',\n" +
+                        "        '查看^javascript:f_op(row-'||rownum||')^_self')\n" +
+                        "         op\n" +
+
+                        "column-type": "link", // link为链接
