aa9c2a0f9c2bd20f99e0a5261f08ed96dc06d0d7
chaodeng <645111208@qq.com>
2018-11-19 17:01:00 +0800

    finle

--- DEV/springboot-react/my-app/src/index.js
+++ DEV/springboot-react/my-app/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
@@ -76,6 +74,17 @@ class Game extends React.Component {
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
 
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -93,7 +102,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
