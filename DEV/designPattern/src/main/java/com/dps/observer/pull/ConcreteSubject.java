832b1791f58fbc76436d59146c62300a5e0356f9
chao.deng <645111208@qq.com>
2018-12-04 23:26:48 +0800

    update obsever

---
+++ DEV/designPattern/src/main/java/com/dps/observer/pull/ConcreteSubject.java
@@ -0,0 +1,21 @@
+package com.dps.observer.pull;
+
+/**
+ * @author dengchao
+ * @date 2018/12/4 23:25
+ */
+public class ConcreteSubject extends Subject{
+
+    private String state;
+
+    public String getState() {
+        return state;
+    }
+
+    public void change(String newState){
+        state = newState;
+        System.out.println("主题状态为：" + state);
+        //状态发生改变，通知各个观察者
+        this.nodifyObservers();
+    }
+}
---
+++ DEV/designPattern/src/main/java/com/dps/observer/pull/Subject.java
@@ -0,0 +1,43 @@
+package com.dps.observer.pull;
+
+import com.dps.observer.push.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dengchao
+ * @date 2018/12/4 10:08
+ */
+public abstract class Subject {
+    /**
+     * 用来保存注册的观察者对象
+     */
+    private    List<Observer> list = new ArrayList<Observer>();
+    /**
+     * 注册观察者对象
+     * @param observer    观察者对象
+     */
+    public void attach(Observer observer){
+
+        list.add(observer);
+        System.out.println("Attached an observer");
+    }
+    /**
+     * 删除观察者对象
+     * @param observer    观察者对象
+     */
+    public void detach(Observer observer){
+
+        list.remove(observer);
+    }
+    /**
+     * 通知所有注册的观察者对象
+     */
+    public void nodifyObservers(){
+
+        for(Observer observer : list){
+            observer.update(this);
+        }
+    }
+}
